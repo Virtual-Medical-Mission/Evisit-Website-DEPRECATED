e7e983afb48344662f9bbf015b1344784a030bbc
HypeByte <hypebyteprogrammer@gmail.com>
2023-07-04 00:20:30 -0700

    debugging index

--- index.php
+++ index.php
@@ -1,51 +1,53 @@
 <?php
+//
+////webpage configuration settings here:
+//$config = [
+//
+//    'name' => 'Register',
+//    'logo' => 'private/assets/img/logo.png',
+//    'stylesheets' => [
+//
+//        'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+//        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css',
+//        'private/assets/css/style.css',
+//        'https://fonts.googleapis.com/css2?family=Heebo:wght@500&family=Montserrat&family=Prompt:wght@700&family=Roboto:wght@500&family=Work+Sans:wght@500&display=swap'
+//
+//    ]
+//
+//];
+//
+////Part of the advanced style tag loader system setup using PHP, code is in header.temp.php
+////For every tag or class you want to add a style to, just make a new array element and then add the styles to that
+////You don't need to add a ';' after each style, the loader does it for you
+//$styles = [
+//
+//    'body' => [
+//
+//            'background-color: #f5f5f5'
+//
+//    ]
+//
+//];
+//
+////This is just an addition I added to make it easier to add and load styles faster and more efficiently to the page
+////If you don't prefer it you don't have to use it, it is an optional feature
+//
+//
+//require_once 'private/init.php';
+//
+//?>
+<!---->
+<?php // require_once 'private/temps/header.temp.php' ?>
+<!---->
+<!---->
+<!---->
+<!--<div class="fs-1 text-center bg-primary pt-5 pb-5 text-light" style="font-family: 'Roboto', sans-serif;">Register:</div>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<?php // require_once 'private/temps/footer.temp.php' ?>
 
-//webpage configuration settings here:
-$config = [
-
-    'name' => 'Register',
-    'logo' => 'private/assets/img/logo.png',
-    'stylesheets' => [
-
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css',
-        'private/assets/css/style.css',
-        'https://fonts.googleapis.com/css2?family=Heebo:wght@500&family=Montserrat&family=Prompt:wght@700&family=Roboto:wght@500&family=Work+Sans:wght@500&display=swap'
-
-    ]
-
-];
-
-//Part of the advanced style tag loader system setup using PHP, code is in header.temp.php
-//For every tag or class you want to add a style to, just make a new array element and then add the styles to that
-//You don't need to add a ';' after each style, the loader does it for you
-$styles = [
-
-    'body' => [
-
-            'background-color: #f5f5f5'
-
-    ]
-
-];
-
-//This is just an addition I added to make it easier to add and load styles faster and more efficiently to the page
-//If you don't prefer it you don't have to use it, it is an optional feature
-
-
-require_once 'private/init.php';
-
-?>
-
-<?php  require_once 'private/temps/header.temp.php' ?>
-
-
-<!-- Start Registration Form Here !-->
-
-<div class="fs-1 text-center bg-primary pt-5 pb-5 text-light" style="font-family: 'Roboto', sans-serif;">Register:</div>
-
-
-
-
-
-<?php  require_once 'private/temps/footer.temp.php' ?>
+<?php
+echo 'hello world';
