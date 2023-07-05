5f99744878c6d4aa149864d6f424ffc1b523d63e
HypeByte <hypebyteprogrammer@gmail.com>
2023-07-05 16:26:21 -0700

    updated sql users table script for some new user table columns

--- sql/users.sql
+++ sql/users.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users
 (
     id bigint(20) NOT NULL AUTO_INCREMENT,
-    full_name VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    username VARCHAR(255),
     gender VARCHAR(255),
     DOB TEXT,
     password VARCHAR(255),
