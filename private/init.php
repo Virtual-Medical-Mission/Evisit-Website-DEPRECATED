1b04e94cb578154a730894fb2c1bcda4cb81b38a
HypeByte <hypebyteprogrammer@gmail.com>
2023-07-05 08:24:40 -0700

    debugging init by removing relative path

--- private/init.php
+++ private/init.php
@@ -4,11 +4,10 @@
 session_start();
 ob_start();
 
-const PRIVATE_PATH = __DIR__;
 
-//smart relative path based loading allows any file in this project to get all the dependencies from init.php
-require_once PRIVATE_PATH . '\database\db_credentials.php';
-require_once PRIVATE_PATH . '\database\database.php';
-require_once PRIVATE_PATH . '\server.php';
+require_once 'database/db_credentials.php';
+require_once 'database/database.php';
+require_once 'query.php';
+require_once 'server.php';
 
 $evisit_db = db_connect();
