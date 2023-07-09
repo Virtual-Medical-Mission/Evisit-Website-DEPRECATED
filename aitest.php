9731fa80f181aaa668a9eb12764aab67b9aa10af
HypeByte <hypebyteprogrammer@gmail.com>
2023-07-09 16:26:28 -0700

    done with ai testing

--- aitest.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'private/init.php';
-require __DIR__ . '/vendor/autoload.php'; // remove this line if you use a PHP Framework.
-use Orhanerday\OpenAi\OpenAi;
-
-echo 'hello world';
