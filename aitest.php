da55592fb2405564e13806a45b86d1d60af4e1ee
HypeByte <hypebyteprogrammer@gmail.com>
2023-07-09 16:22:13 -0700

    running another test

--- aitest.php
+++ aitest.php
@@ -3,5 +3,4 @@ require_once 'private/init.php';
 require __DIR__ . '/vendor/autoload.php'; // remove this line if you use a PHP Framework.
 use Orhanerday\OpenAi\OpenAi;
 
-$ai_key = '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';
-$HPI_AI = new OpenAi($ai_key);
+echo 'hello world';
