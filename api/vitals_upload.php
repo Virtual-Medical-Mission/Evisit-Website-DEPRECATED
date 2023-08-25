17ead334c53eb85963b3c87ac473c41a8e8c7d34
HypeByte <hypebyteprogrammer@gmail.com>
2023-08-24 21:12:24 -0700

    vitals upload api{

--- api/vitals_upload.php
+++ api/vitals_upload.php
@@ -1,3 +1,18 @@
 <?php
 
+require_once '../private/init.php';
 
+if( is_post_request() ) {
+
+    if( isset($_POST['vitals']) ) {
+        var_dump($_POST['vitals']);
+//        $vitals = explode(' ', $_POST['EKG']);
+//        $spo2 = $vitals[0];
+//        $heartrate = $vitals[1];
+//        $BP = $vitals[2];
+//        $temperature = $vitals[3];
+//        $ekg = explode(',', $vitals[4]);
+
+    }
+
+}
