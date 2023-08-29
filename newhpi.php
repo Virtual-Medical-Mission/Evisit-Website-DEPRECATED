f24052e6cc546d2b4cb813e35bc92eafe17449f1
HypeByte <hypebyteprogrammer@gmail.com>
2023-08-28 17:23:59 -0700

    new HPI Form based on Questionnaire Framework

---
+++ newhpi.php
@@ -0,0 +1,99 @@
+<?php
+require_once 'private/init.php';
+require_once 'Questionnaire/Question.php';
+require_once 'Questionnaire/Form.php';
+require_once 'Questionnaire/RadioCheck.php';
+require_once 'Questionnaire/TextBox.php';
+require_once 'Questionnaire/Select.php';
+require_once 'Questionnaire/Calendar.php';
+require_once 'Questionnaire/Questionnaire.php';
+require_once 'Questionnaire/Node.php';
+require_once 'Questionnaire/DxTx.php';
+
+use Questionnaire\{Questionnaire, Question, Form, RadioCheck, TextBox, Select, Calendar, Node, DxTx};
+
+$hpi = new Questionnaire('hpi', [
+
+    new Form(
+
+        'page1',
+        [
+            new Select('Primary Language', '1', 'English,Swahili', 'SELECT_DEFAULT'),
+            new Select('Are you part of a tribe?', '2', "Yes,No", "SELECT_DEFAULT"),
+            new TextBox('Where have you lived for the past 6 weeks?', '3', "TEXTBOX_DEFAULT"),
+            new Calendar('When did you start living in that location?', '4', 'CALENDAR_DEFAULT')
+        ]
+
+    ),
+
+    new Form(
+
+        'page2',
+        [
+            new TextBox('Do you have any chronic medical conditions? If so, please list them:', '1', 'TEXTBOX_DEFAULT'),
+            new TextBox('Do you take any medications? If so, please list them:', '2', 'TEXTBOX_DEFAULT'),
+            new TextBox('Have you had any surgeries in the past? If so, please list them:', '3', 'TEXTBOX_DEFAULT'),
+            new TextBox('Do you have any allergies? If so, please list them:', '4', 'TEXTBOX_DEFAULT'),
+            new Select('Have you ever been hospitalized?', '5', 'No,Yes', 'SELECT_DEFAULT')
+        ]
+
+    ),
+
+    new Form(
+        'page3',
+        [
+            new Select('Do you have access to clean water?', '1', 'Yes,No', 'SELECT_DEFAULT'),
+            new TextBox('What is your primary method of transportation?', '2', 'TEXTBOX_DEFAULT'),
+            new Select('Are you up to date on all of your immunizations?', '3', 'Yes,No', 'SELECT_DEFAULT'),
+            new TextBox('Do you have any dietary restrictions? If so, please list them:', '4', 'TEXTBOX_DEFAULT'),
+        ]
+    ),
+
+    new Form(
+        'page4',
+        [
+            new Select('How often do you smoke?', '1', 'Never,Occasionally,Daily', "SELECT_DEFAULT"),
+            new Select('How often do you drink alcohol?', '2', 'Never,Occasionally,Daily', "SELECT_DEFAULT"),
+            new Select('How often do you use recreational drugs?', '3', 'Never,Occasionally,Daily', "SELECT_DEFAULT"),
+            new TextBox('Do you engage in physical activities? If so, please list them:', '4', 'TEXTBOX_DEFAULT'),
+            new TextBox('Do you have any family history of disease? If so, please list them:', '5', 'TEXTBOX_DEFAULT')
+
+        ]
+    )
+
+]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/private/assets/css/style.css" />
+    <link rel="stylesheet" href="/private/assets/css/questionnaire.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <title>Survey</title>
+</head>
+
+<body>
+<?php vmm_banner('E-visit Survey'); ?>
+<section class="shadow contact-clean" style="background-color: antiquewhite">
+
+    <form class="border rounded border-secondary shadow-lg" action="newhpi.php" method="post">
+
+        <?php
+
+        $hpi->render();
+
+        ?>
+
+
+
+    </form>
+
+</section>
+
+<?php include 'private/temps/footer.temp.php'?>
--- staffportal/index.php
+++ staffportal/index.php
@@ -11,33 +11,7 @@
     <title>Staff Dashboard</title>
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-2 bg-dark">
-            <div class="sidebar">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Employees</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Reports</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Settings</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-10">
-            <div class="content">
-                <!-- Content goes here -->
-            </div>
-        </div>
-    </div>
-</div>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
