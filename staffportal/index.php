c004153c583916766866cc646fe694aa83759ada
Alex Vizz <136540252+AlexVizz@users.noreply.github.com>
2023-09-18 20:34:41 -0700

    Basic doctor type view
    still working

--- staffportal/index.php
+++ staffportal/index.php
@@ -1,17 +1,36 @@
-
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <title>Staff Dashboard</title>
+    <title>Doctor's View</title>
 </head>
 <body>
-
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <img src="private/assets/img/Usher.jpg" class="img-fluid rounded" alt="Patient Image">
+        </div>
+        <div class="col-md-9">
+            <h2>Patient Information</h2>
+            <table class="table table-bordered">
+                <tr>
+                    <th>Name</th>
+                    <td>John Doe</td>
+                </tr>
+                <tr>
+                    <th>Age</th>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <th>Gender</th>
+                    <td>Male</td>
+                </tr
+            </table>
+        </div>
+    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
