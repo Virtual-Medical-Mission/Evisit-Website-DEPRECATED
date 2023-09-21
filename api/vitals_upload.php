00f4ba4c4bb02e4e26fc15af3e127f92a0c6f5d9
NihalSekhon <135891412+NihalSekhon@users.noreply.github.com>
2023-09-21 11:48:28 -0400

    Update vitals_upload.php to upload ip to ekg for testing

--- api/vitals_upload.php
+++ api/vitals_upload.php
@@ -14,12 +14,11 @@ if( is_post_request() ) {
         $dataip = $vitals[5];
         
         
-        $sql = 'INSERT INTO vitals (oxsat, heartrate, BP, temp, EKG, DATAIP ) VALUES (';
+        $sql = 'INSERT INTO vitals (oxsat, heartrate, BP, temp, EKG, VALUES (';
         $sql .= "'" . db_escape($evisit_db, $spo2) . "',";
         $sql .= "'" . db_escape($evisit_db, $heartrate) . "',";
         $sql .= "'" . db_escape($evisit_db, $BP) . "',";
         $sql .= "'" . db_escape($evisit_db, $temperature) . "',";
-        $sql .= "'" . db_escape($evisit_db, $ekg) . "',";
         $sql .= "'" . db_escape($evisit_db, $dataip) . "')";
         echo $dataip;
         
