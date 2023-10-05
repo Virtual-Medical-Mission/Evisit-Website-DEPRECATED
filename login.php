62aba5df4e2b26aea5bdc18fac3f9cb29a81a441
NihalSekhon <135891412+NihalSekhon@users.noreply.github.com>
2023-10-05 18:42:25 -0400

    made chnge to test vitalslink integration

--- login.php
+++ login.php
@@ -7,6 +7,7 @@ if(is_post_request()) {
     $login_result = login_user($_POST);
     if($login_result) {
         $_SESSION['hpi_ready'] = 'true';
+        header("Location: alert:hello");
         redirect_to('hpi.php');
     } else {
         $error = true;
