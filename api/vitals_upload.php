2594069af97a2858b022772d3b03788246a5cb90
HypeByte <hypebyteprogrammer@gmail.com>
2023-10-21 13:41:05 -0700

    added vitals pairing

--- api/vitals_upload.php
+++ api/vitals_upload.php
@@ -24,9 +24,22 @@ if( is_post_request() ) {
         
         $result = mysqli_query($evisit_db, $sql);
         confirm_result_set($result);
+
+        $vid = mysqli_insert_id($evisit_db);
+
         echo 'goodsend';
 
+        //Get the last appointment id that has the ip
+        $sql2 = "SELECT * FROM appointments WHERE ip = '" . db_escape($evisit_db, $ip) . "'ORDER BY id DESC LIMIT 1";
+        $result2 = mysqli_query($evisit_db, $sql2);
+        $apid = mysqli_fetch_assoc($result2)['id'];
 
+        //Update the appointment with the vitals id
+        $sql3 = "UPDATE appointments SET vid = '" . db_escape($evisit_db, $vid) . "' WHERE id = '" . db_escape($evisit_db, $apid) . "'";
+        $result = mysqli_query($evisit_db, $sql3);
+        confirm_result_set($result);
+
+        echo 'Vitals have been successfully linked to user';
     }
 
 }
