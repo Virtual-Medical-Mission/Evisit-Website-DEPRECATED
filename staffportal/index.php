e940e64c1edb141bcddc14eb1d140db5fc3b80e1
HypeByte <hypebyteprogrammer@gmail.com>
2023-10-21 20:18:35 -0700

    finished appointment table with links to view them

--- staffportal/index.php
+++ staffportal/index.php
@@ -1,3 +1,15 @@
+<?php
+require_once "../private/init.php";
+
+global $evisit_db;
+
+$sql = "SELECT * FROM appointments";
+$result = mysqli_query($evisit_db, $sql);
+confirm_result_set($result);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,37 +19,47 @@
     <title>Doctor's View</title>
 </head>
 <body>
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-md-12">
-            <h1 class="text-center">Hello Dr. John Smith</h1>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Hello Dr. Ayush Moghe</h1>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <h1 class="text-center">Doctor's Patient List</h1>
-            <table class="table table-bordered">
-                <thead>
-                <tr>
-                    <th>Patient ID</th>
-                    <th>Patient Name</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td><a href="index.php">1</a></td>
-                    <td>John Doe</td>
-                </tr>
-                <tr>
-                    <td><a href="index.php">2</a></td>
-                    <td>Jane Smith</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Appointment's List</h1>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Appointment ID</th>
+                            <th>Patient Name</th>
+                            <th>Appointment Start Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while($row = mysqli_fetch_assoc($result)): ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+                            <?php
+                                //Gets the username of the patient for the appointment
+                                $sql2 = "SELECT * FROM users WHERE id = " . $row['uid'];
+                                $result2 = mysqli_query($evisit_db, $sql2);
+                                confirm_result_set($result2);
+                                $username = mysqli_fetch_assoc($result2)['username'];
+                            ?>
+
+                            <tr>
+                                <td><a href= <?php echo ("AppointmentView.php?apid=" . $row['id']) ?>?><?php echo $row['id']; ?></a></td>
+                                <td><?php echo $username; ?></td>
+                                <td><?php echo $row['checkedin']; ?></td>
+                            </tr>
+
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
