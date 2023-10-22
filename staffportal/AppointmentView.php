1239fb42c8ec4c8e5742a0992900cb3f5c055c0d
NihalSekhon <135891412+NihalSekhon@users.noreply.github.com>
2023-10-21 22:40:07 -0400

    Update AppointmentView.php to remove debug fuction that i fogot ;(

--- staffportal/AppointmentView.php
+++ staffportal/AppointmentView.php
@@ -1,20 +1,15 @@
 <?php
 require_once '../private/init.php';
 $appviewing = $_GET['apid'];
-debug_to_console($appviewing);
 $sql = "SELECT * FROM appointments WHERE id = ' " . $appviewing . " ' ";
 $result = mysqli_query($evisit_db, $sql);
 $appointmentRow = mysqli_fetch_assoc($result);
-debug_to_console($appointmentRow['uid']);
 $sql2 = "SELECT * FROM users WHERE id = ' " . $appointmentRow['uid'] . " ' ";
 $result2 = mysqli_query($evisit_db, $sql2);
 $userRow = mysqli_fetch_assoc($result2);
-debug_to_console($userRow['first_name']);
 $sql3 = "SELECT * FROM embedded_responses WHERE apid = ' " . $appviewing . " ' ";
 $result3 = mysqli_query($evisit_db, $sql3);
-debug_to_console($appointmentRow['vid']);
 if (($appointmentRow['vid'] !== '-1') && ($appointmentRow['vid'] !== null ) ) {
-    debug_to_console("seraching for vitals");
     $sql4 = "SELECT * FROM vitals WHERE id = ' " . $appointmentRow['vid'] . " ' ";
     $result4 = mysqli_query($evisit_db, $sql4);
     $vitalsRow = mysqli_fetch_assoc($result4);
@@ -28,10 +23,6 @@ if (($appointmentRow['vid'] !== '-1') && ($appointmentRow['vid'] !== null ) ) {
     $BP = "not found";
     $temp = "not found";
 }
-//while($reponsesRow = mysqli_fetch_assoc($result3)) :
-  //  debug_to_console($reponsesRow['question']);
-  //  debug_to_console($reponsesRow['response']);
-//endwhile;
 ?>
 
 
