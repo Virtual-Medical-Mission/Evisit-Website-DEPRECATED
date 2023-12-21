d0a97ab942087e6a615097d635267ddffdbf4ed1
NihalSekhon <135891412+NihalSekhon@users.noreply.github.com>
2023-12-21 14:20:39 -0800

    Update AppointmentView.php bugfix

--- staffportal/AppointmentView.php
+++ staffportal/AppointmentView.php
@@ -142,6 +142,6 @@ if (($appointmentRow['vid'] !== '-1') && ($appointmentRow['vid'] !== null ) ) {
     </div>
 </div>
 
-<script // src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
